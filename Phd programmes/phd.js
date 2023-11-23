9b2668a4fad036173489d87c297890083536f838
unknown <adipahosei@gmail.com>
2023-11-23 19:41:27 +0000

    Initial commit to git hub

---
+++ Phd programmes/phd.js
@@ -0,0 +1,9 @@
+<script>
+querySelector('.backtop').addEventListener('click', function() {
+    // Scroll to the top of the page
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Smooth scrolling animation
+    })
+})
+</script>
